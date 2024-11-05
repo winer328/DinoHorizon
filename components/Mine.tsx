663b1ddbf5af13e3da4b698a03f0678fcee019fc
thedefidude18 <102023568+thedefidude18@users.noreply.github.com>
2024-11-05 16:34:09 +0100

    Update Mine.tsx

--- components/Mine.tsx
+++ components/Mine.tsx
@@ -6,26 +6,26 @@ export default function GameBanners() {
 
   const slides = [
     {
-      title: "RUN, JUMP & EARN DINO",
+      title: "Tap and Earn DINOH",
       buttonText: "Tap now 🦕",
       backgroundImage: "url('https://raw.githubusercontent.com/RollupRadar/project23/main/images/tappo2.svg')",
     },
     {
       title: "Play and Win a ps5",
       buttonText: "Invite",
-      backgroundImage: "url('https://raw.githubusercontent.com/RollupRadar/project23/main/images/tappo2.svg')",
+      backgroundImage: "url('https://raw.githubusercontent.com/thedefidude18/dinohorizon/main/images/poster-battle.jpg')",
     },
     {
-      title: "CHALLENGE YOUR FRIENDS",
-      buttonText: "Explore",
-      backgroundImage: "url('https://raw.githubusercontent.com/RollupRadar/project23/main/images/tappo2.svg')",
+      title: "Dino Dash",
+      buttonText: "Coming Soon",
+      backgroundImage: "url('https://raw.githubusercontent.com/thedefidude18/dinohorizon/main/images/dinodashh.jpg')",
     },
   ];
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
-    }, 3000); // Change slide every 3 seconds
+    }, 4000); // Change slide every 4 seconds
     return () => clearInterval(interval);
   }, []);
 
@@ -33,7 +33,7 @@ export default function GameBanners() {
     <div
       className="flex flex-col items-center min-h-screen"
       style={{
-        background: "linear-gradient(to bottom, #575EFF, #0ECBFF 94%)",
+        background: "linear-gradient(to bottom, #fffff, #0ECBFF 94%)",
         fontFamily: "'ZCOOL KuaiLe', sans-serif",
         padding: "2vw",
       }}
@@ -61,15 +61,15 @@ export default function GameBanners() {
             fontFamily: "'ZCOOL KuaiLe', sans-serif",
           }}
         >
-          Welcome Dino Horizon.
+          Welcome to Dino Horizon.
         </h1>
       </div>
 
       {/* Upper Banner with Slider */}
       <div
         style={{
-          width: "92vw",
-          height: "46vh",
+          width: "94vw",
+          height: "75vh",
           backgroundImage: slides[currentSlide].backgroundImage,
           backgroundSize: "cover",
           backgroundPosition: "center",
@@ -81,35 +81,17 @@ export default function GameBanners() {
           alignItems: "flex-start",
           color: "#ffffff",
           position: "relative",
-          marginBottom: "1vh", // Reduced space between banners
-          padding: "4vw",
+          marginBottom: "0.8vh", // Reduced space between banners
+          padding: "1vw",
         }}
       >
-        {/* Overlay for Title and Button */}
-        <div
-          style={{
-            background: "linear-gradient(to left, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.6))",
-            width: "100%",
-            borderRadius: "5vw",
-            padding: "3vw",
-          }}
-        >
-          <h2
-            style={{
-              fontSize: "5vw",
-              fontWeight: "bold",
-              margin: "0 0 2vw 2vw",
-              fontFamily: "'ZCOOL KuaiLe', sans-serif",
-            }}
-          >
-            {slides[currentSlide].title}
-          </h2>
+        
           <button
             onClick={() => setButtonText("Loading...")}
             style={{
               padding: "2vw 5vw",
               background: "linear-gradient(to bottom, #FCD113, #F6BA06)",
-              color: "#ffffff",
+              color: "#000",
               borderRadius: "2vw",
               fontSize: "4vw",
               fontWeight: "bold",
@@ -149,62 +131,8 @@ export default function GameBanners() {
           ))}
         </div>
       </div>
-
-      {/* Lower Banner */}
-      <div
-        style={{
-          width: "92vw",
-          height: "32vh",
-          backgroundImage: "url('https://raw.githubusercontent.com/RollupRadar/project23/main/images/tappo2.svg')",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          borderRadius: "5vw",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-end",
-          alignItems: "flex-start",
-          color: "#fff",
-          padding: "4vw",
-          marginTop: "1vh", // Reduced space between banners
-        }}
-      >
-        {/* Static Content for Lower Banner */}
-        <div
-          style={{
-            background: "linear-gradient(to left, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.6))",
-            width: "100%",
-            borderRadius: "5vw",
-            padding: "3vw",
-          }}
-        >
-          <h2
-            style={{
-              fontSize: "5vw",
-              fontWeight: "bold",
-              margin: "0 0 1vw 2vw",
-              fontFamily: "'ZCOOL KuaiLe', sans-serif",
-            }}
-          >
-            Jump over obstacles.
-          </h2>
-          <button
-            style={{
-              padding: "2vw 5vw",
-              background: "linear-gradient(to bottom, #FCD113, #F6BA06)",
-              color: "#ffffff",
-              borderRadius: "2vw",
-              fontSize: "4vw",
-              fontWeight: "bold",
-              border: "none",
-              marginLeft: "2vw",
-              fontFamily: "'ZCOOL KuaiLe', sans-serif",
-            }}
-          >
-            {buttonText}
-          </button>
-        </div>
-      </div>
-    </div>
+      
+   
   );
 }
+
